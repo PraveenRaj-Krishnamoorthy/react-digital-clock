aebd72ffa15ca6e1cb7b3deee47f3024e2e99676
PraveenRaj-Krishnamoorthy <praveenraj.krishnamoorthy@outlook.com>
2024-04-20 22:12:13 +0530

    Digital
    Some codes still pending

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+export default function App() {
+  const [Time, setTime] = useState(new Date())
+  useEffect(() => {
+    const Timer = setInterval(() => { return (setTime(new Date())) }, 1000)
+    // return annonomus is really important when clear interval!
+    return () => (
+      clearInterval(Timer)
+    )
+  }, [])
+  // Or Use the below interval //
+  // setInterval(()=>{
+  //   setTime(new Date())
+  // },1000)
+  const Hours = (h) => {
+    return (
+      h > 12 ? h - 12 : h
+    )
+  }
+  const Minutes = (m) => {
+    return m < 10 ? "0" + m : m
+  }
+  const Seconds = (s) => {
+    return s < 10 ? "0" + s : s
+  }
+  return (
+    <>
+      <main>
+        <h1>{Time.getDate()} - {Time.getMonth() + 1} - {Time.getFullYear()}</h1>
+        <div className='time'>
+          <h1>{Hours(Time.getHours()) < 10 ? "0" + Hours(Time.getHours()) : Hours(Time.getHours())}</h1>
+          <h1>{Minutes(Time.getMinutes())}</h1>
+          <h1>{Seconds(Time.getSeconds())}</h1>
+        </div>
+      </main>
+    </>
+  )
+}
